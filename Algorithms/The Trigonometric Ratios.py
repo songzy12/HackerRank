5c1358e48d3a194195d8f742e953fbb40853e774
songzy <songzy.thu@qq.com>
2016-09-04 15:54:14 +0800

    (a,b) -> (x,y) possible path

--- Algorithms/The Trigonometric Ratios.py
+++
@@ -1,6 +0,0 @@
-f=[1,1,-2,-6,24,120,-720,-5040,40320,362880]
-N = int(input())
-for n in range(N):
-    print('%.3f\n%.3f\n'%(sum([3**i/f[i] for i in range(1,10,2)]), sum([3**i/f[i] for i in range(0,9,2)])))
-    map(lambda x:print(x), input())
-    map(lambda x:print('%.3f\n%.3f\n'%(sum([x**i/f[i] for i in range(1,10,2)]), sum([x**i/f[i] for i in range(0,9,2)]))), float(input()))
---
+++ Practice/Mathematics/Possible Path.cpp
@@ -0,0 +1,23 @@
+/* Adam正站在一个无限大的二维网格的(a,b)点。他想知道他能否到达(x,y)点。他唯一可做的操作是从某个点 (a,b)，移动到(a+b,b), (a,a+b), (a-b,b), 或者 (a,a-b) 。 他允许到二维网格的任何点，点的 X(或者 Y)坐标可以为正、负，0。
+ *
+ * 告诉Adam它能否到达(x,y)点
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int t;
+    cin>>t;
+    while (t--) {
+        int x, y, w, z;
+        cin>>x>>y>>w>>z;
+        cout<<(__gcd(x,y) == __gcd(w,z) ? "YES" : "NO")<<endl;
+    }
+    return 0;
+}
