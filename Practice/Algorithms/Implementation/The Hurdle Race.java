ce7c2c1816d7a29f9e1e8263b79b0ded59694597
unknown <songzy.thu@qq.com>
2017-07-07 18:24:41 +0800

    .

---
+++ Practice/Algorithms/Implementation/The Hurdle Race.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        for(int height_i=0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+        // your code goes here
+        int max = Arrays.stream(height).max().getAsInt();
+        System.out.println(max > k ? max - k : 0);
+    }
+}
