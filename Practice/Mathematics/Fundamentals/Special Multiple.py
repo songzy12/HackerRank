822cd3e71ff3a6e31f334b4d70a0ef9ad66e8832
songzy <songzy.thu@qq.com>
2017-12-30 10:03:02 +0800

    .

---
+++ Practice/Mathematics/Fundamentals/Special Multiple.py
@@ -0,0 +1,15 @@
+# cannot figure out the math here
+
+
+def solve(N):
+    n = 1
+    while True:
+        temp = int(bin(n)[2:].replace('1','9'))
+        if temp % N == 0:
+            return temp
+        n += 1
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    print(solve(N))
