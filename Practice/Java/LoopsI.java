7890c9f4ee684e367e48d4ee6b372059c6336f4f
Zhengyang Song <songzy_thu@163.com>
2023-03-11 15:07:46 +0800

    Java Loops

---
+++ Practice/Java/LoopsI.java
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/java-loops-i/
+
+import java.io.*;
+
+
+
+public class LoopsI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", N, i, N * i);
+        }
+
+        bufferedReader.close();
+    }
+}
