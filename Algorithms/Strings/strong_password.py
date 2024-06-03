8988f3a4ad7307167c7204def0c6f246c95dedc5
Zhengyang Song <songzy_thu@163.com>
2024-06-03 18:04:10 +0800

    strong password

---
+++ Algorithms/Strings/strong_password.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/strong-password/problem?isFullScreen=true
+
+
+def minimumNumber(unused, password):
+    numbers = "0123456789"
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    special_characters = "!@#$%^&*()-+"
+
+    def needsOne(password, charset):
+        for t in password:
+            if t in charset:
+                return 0
+        return 1
+
+    res = needsOne(password, numbers) + needsOne(password, lower_case) + \
+         needsOne(password, upper_case) + needsOne(password, special_characters)
+    return max(res, 6 - len(password))
