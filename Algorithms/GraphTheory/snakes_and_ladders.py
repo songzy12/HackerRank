c9ac735a4f2ccfc93bd2cf26d14ca62a8f0ce661
Zhengyang Song <songzy_thu@163.com>
2024-06-16 22:05:43 +0800

    Add placeholder for the snakes_and_ladders problem.

--- Algorithms/GraphTheory/snakes_and_ladders.py
+++ Algorithms/GraphTheory/snakes_and_ladders.py
@@ -1,5 +1,9 @@
 # https://www.hackerrank.com/challenges/the-quickest-way-up/problem?isFullScreen=true
-
+#
+# How to run:
+#   export OUTPUT_PATH=/tmp/output.txt
+#   python3 snakes_and_ladders.py < in.txt
+#   cat $OUTPUT_PATH
 import math
 import os
 import random
@@ -9,31 +13,23 @@ import sys
 
 def quickestWayUp(ladders, snakes):
     # TODO: Write your code here
-    pass
+    return 0
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    with open(os.environ['OUTPUT_PATH'], 'w') as fptr:
+        t = int(input().strip())
 
-    t = int(input().strip())
+        for t_itr in range(t):
+            n = int(input().strip())
+            ladders = []
+            for _ in range(n):
+                ladders.append(list(map(int, input().rstrip().split())))
 
-    for t_itr in range(t):
-        n = int(input().strip())
+            m = int(input().strip())
+            snakes = []
+            for _ in range(m):
+                snakes.append(list(map(int, input().rstrip().split())))
 
-        ladders = []
-
-        for _ in range(n):
-            ladders.append(list(map(int, input().rstrip().split())))
-
-        m = int(input().strip())
-
-        snakes = []
-
-        for _ in range(m):
-            snakes.append(list(map(int, input().rstrip().split())))
-
-        result = quickestWayUp(ladders, snakes)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+            result = quickestWayUp(ladders, snakes)
+            fptr.write(str(result) + '\n')
