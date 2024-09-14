78fcf896fbb09e0e0514dd81c166284b62f9b32b
Zhengyang <songzy_thu@163.com>
2024-09-14 09:24:03 +0800

    Rewrite the editorial of Maximum Palindrome.

--- Algorithms/Strings/maximum_palindromes.py
+++ Algorithms/Strings/maximum_palindromes.py
@@ -1,14 +1,24 @@
 # https://www.hackerrank.com/challenges/maximum-palindromes/problem?isFullScreen=true
 #
 # Two steps:
-# 1. For give l and r, compute a map with 26 keys, from each letter to its count.
+# 1. For give l and r, compute a map of each char to its count.
 # 2. Then, for such a map, compute the number of palindromes.
-#    a. init array_even[] and count_mid
-#    b. first, for each key k, with count c[k]
-#       i.  if c[k] // 2 > 0, array_even.append(c[k] // 2)
-#       ii. if c[k] % 2 === 1, count_mid += 1
-#    c. the answer would be count_mid * combination(array_even)
-#       i. here combination(array_even) = (\sum array_even[i])! / \prod (array_even[i])!
+#    a. We count the possiblities that the chars can be put in the middle and on both sides.
+#    b. For each key k, with count c[k]
+#       i.  if c[k] // 2 > 0, then c[k] // 2 can be put symmetrically on both sides
+#       ii. if c[k] % 2 === 1, then this char can also be put in the middle
+#    c. To construct a maximum palindorme, there are 2 steps:
+#       i. pick one from the chars that can be put in the middle
+#       ii. choose a combination of the chars that can be put on the sides
+#       So the answer would be len(char_mid) * combination(char_sides)
+#       Here combination(char_sides) = (\sum char_sides[i])! / \prod (char_sides[i])!
+# 3. To compute the module, one can use Fermat's Little Theorem, a.k.a, a^{-1} = a^{p-2} (mod p)
+#
+# References: 
+# 1. https://en.wikipedia.org/wiki/Prefix_sum
+# 2. https://en.wikipedia.org/wiki/Binomial_coefficient#Generalization_to_multinomials
+# 3. https://en.wikipedia.org/wiki/Fermat%27s_little_theorem
+# 4. https://en.wikipedia.org/wiki/Exponentiation_by_squaring
 
 import os
 import string
@@ -25,7 +35,7 @@ inv_factors = [1 for i in range(MAX_LEN)]
 def compute_pow(i, p):
     if p == 0:
         return 1
-    tmp = compute_pow(i, p // 2)**2 % MOD
+    tmp = compute_pow(i, p // 2) ** 2 % MOD
     if p % 2 == 0:
         return tmp
     return tmp * i % MOD
@@ -79,24 +89,20 @@ def answerQuery(l, r):
     return compute(mid_count, side_counts)
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     s = input()
-
     initialize(s)
 
     q = int(input().strip())
-
     for q_itr in range(q):
         first_multiple_input = input().rstrip().split()
-
         l = int(first_multiple_input[0])
-
         r = int(first_multiple_input[1])
 
         result = answerQuery(l, r)
 
-        fptr.write(str(result) + '\n')
+        fptr.write(str(result) + "\n")
 
     fptr.close()
