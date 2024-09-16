531f1e87a3581e17f99b45e6a772dbcc8b978316
Zhengyang <songzy_thu@163.com>
2024-09-16 23:47:39 +0800

    Rewrite problem highest value palindrome.

--- Algorithms/Strings/richie_rich.py
+++ Algorithms/Strings/richie_rich.py
@@ -1,12 +1,12 @@
 # https://www.hackerrank.com/challenges/richie-rich/problem?isFullScreen=true
 
 
-def richieRich(s, n, k):
+def highestValuePalindrome(s, n, k):
     i = 0
     j = len(s) - 1
     s = list(s)
 
-    changed = [False for t in range(n)]  # stupid
+    changed = [False for t in range(n)]
     num_changed = 0
     while i < j:
         if s[i] != s[j]:
@@ -21,29 +21,23 @@ def richieRich(s, n, k):
         i += 1
         j -= 1
     if i < j:
-        return -1
+        return "-1"
 
     i = 0
     j = len(s) - 1
     while i < j and num_changed < k:
-        if s[i] != '9':
+        if s[i] != "9":
             if not changed[i] and num_changed + 2 <= k:
-                s[i] = '9'
-                s[j] = '9'
+                s[i] = "9"
+                s[j] = "9"
                 num_changed += 2
             if changed[i] and num_changed + 1 <= k:
-                s[i] = '9'
-                s[j] = '9'
+                s[i] = "9"
+                s[j] = "9"
                 num_changed += 1
         i += 1
         j -= 1
 
     if i == j and num_changed < k:
-        s[i] = '9'
-    return ''.join(s)
-
-
-n, k = [int(t) for t in raw_input().split()]
-s = raw_input().strip()
-result = richieRich(s, n, k)
-print(result)
+        s[i] = "9"
+    return "".join(s)
